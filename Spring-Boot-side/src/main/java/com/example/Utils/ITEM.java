828eb8555224712eed6a0d98331141d2cbf63e7e
huwso <robertagu2004@gmail.com>
2024-04-08 20:37:55 -0500

    Se modifico la consulta de productos

---
+++ Spring-Boot-side/src/main/java/com/example/Utils/ITEM.java
@@ -0,0 +1,30 @@
+package com.example.Utils;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ITEM {
+private String _codigoProducto;
+private int _cantidad;
+@JsonCreator
+public ITEM(String _codigoProducto, int _cantidad) {
+    this._codigoProducto = _codigoProducto;
+    this._cantidad = _cantidad;
+}
+
+public String get_codigoProducto() {
+    return _codigoProducto;
+}
+
+public void set_codigoProducto(String _codigoProducto) {
+    this._codigoProducto = _codigoProducto;
+}
+
+public int get_cantidad() {
+    return _cantidad;
+}
+public void set_cantidad(int _cantidad) {
+    this._cantidad = _cantidad;
+}
+
+}
